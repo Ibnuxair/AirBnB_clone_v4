a274e1841d858fb77f59268e4cb906aaccdc45eb
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-14 14:25:48 +0000

    AirBnB

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,33 +1,14 @@
-// Wait for the DOM to be fully loaded
-$(document).ready(() => {
-    // Variable to store Amenity IDs
-    let amenityIds = [];
+$(document).ready(init);
 
-    // Function to update the h4 tag with the list of Amenities checked
-    const updateAmenities = () => {
-	// Get the h4 tag inside the div with the id "Amenities"
-	const amenitiesH4 = $("#Amenities h4");
-
-	// Update the text content with the list of Amenities checked
-	amenitiesH4.text("Amenities: " + amenityIds.join(", "));
-    };
-
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function () {
-	// Get the data-id and data-name attributes from the checkbox
-	const amenityId = $(this).data("id");
-	const amenityName = $(this).data("name");
-
-	// Check if the checkbox is checked
-	if ($(this).prop("checked")) {
-	    // Add Amenity ID to the variable
-	    amenityIds.push(amenityId);
-	} else {
-	    // Remove Amenity ID from the variable
-	    amenityIds = amenityIds.filter((id) => id !== amenityId);
+function init () {
+    const amenityObj = {};
+    $('.amenities .popover input').change(function () {
+	if ($(this).is(':checked')) {
+	    amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+	} else if ($(this).is(':not(:checked)')) {
+	    delete amenityObj[$(this).attr('data-name')];
 	}
-
-	// Update the h4 tag with the list of Amenities checked
-	updateAmenities();
+	const names = Object.keys(amenityObj);
+	$('.amenities h4').text(names.sort().join(', '));
     });
-});
+}
